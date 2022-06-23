9e063dd6d0517de4e36b5da8f6eb9f57ca2ec7f6
Denis <d.combe@laposte.net>
2022-06-23 09:29:50 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudiScrum0622.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
